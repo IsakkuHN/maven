b4bb1666dffa3d658ac522d6be9e1f3e55b1f537
Nelson Rivera <isaacriverahn7@gmail.com>
2022-09-12 01:57:25 -0600

    Initial

---
+++ src/main/java/com/github/isakkuhn/Main.java
@@ -0,0 +1,10 @@
+package com.github.isakkuhn;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(StringUtils.capitalize("world"));
+    }
+}
